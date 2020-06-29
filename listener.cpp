5c7f68d86fdb4b08799e60393ab561f39ae6d855
babaoriley <helenebrulin@live.fr>
2020-06-29 18:11:32 +0200

    init v1

--- listener.cpp
+++ listener.cpp
@@ -1,14 +1,32 @@
 #include "listener.hpp"
 
+void Listener::set_non_blocking() {
+	//std::cout << m_sock << std::endl;
+	if (fcntl(m_sock, F_SETFL, O_NONBLOCK) < 0) {
+		strerror(errno);
+		exit(EXIT_FAILURE);
+	}
+}
+
 Listener::Listener() {
-	m_ip = NULL;
+	memset((char *) &m_address, 0, sizeof(m_address));
 	m_port = 0;
 	m_sock = 0;
-	m_socket = NULL;
+	memset((char *) &m_connect_list, 0, sizeof(m_connect_list));
 	//set = NULL;
+	m_highsock = 0;
+	m_run = true;
 }
 
 int Listener::init() {
+	int reuse_addr = 1;  /* Used so we can re-bind to our port
+				while a previous connection is still
+				in TIME_WAIT state : TIME_WAIT indicates that server
+				has closed the connection. The connection is being kept around 
+				so that any delayed packets can be matched to the connection 
+				and handled appropriately. The connections will be 
+				removed when they time out within four minutes.*/
+	
 	m_port = 8080; //change according to config file
 
 	/* Obtain a file descriptor for listening socket */
@@ -18,12 +36,52 @@ int Listener::init() {
 		exit(EXIT_FAILURE);
 	}
 
+	/* Allow socket descriptor to be reuseable */
+	/* So that we can re-bind to sock without TIME_WAIT problems, without waiting 4 minutes */
+	/*Indique que les règles permettant la validation des adresses 
+	fournies dans un appel bind(2) doivent autoriser la réutilisation 
+	des adresses locales, même en TIME_WAIT. */
+	setsockopt(m_sock, SOL_SOCKET, SO_REUSEADDR, &reuse_addr,
+		sizeof(reuse_addr));
+
+	set_non_blocking();
+
+	// Bind the ip address and port to a socket
+	m_address.sin_family = AF_INET;
+    m_address.sin_port = m_port; //besoin htons ou atoport??
+    m_address.sin_addr.s_addr = inet_addr("0.0.0.0"); //any address
+ 
+    if (bind(m_sock, (struct sockaddr*) &m_address, sizeof(m_address)) < 0)
+	{
+		strerror(errno);
+		close(m_sock);
+		exit(EXIT_FAILURE);
+	}
+
+	/* This socket will be for listening */
+	/*marque la socket référencée par sockfd comme une socket passive, 
+	c'est-à-dire comme une socket qui sera utilisée pour accepter les demandes 
+	de connexions entrantes en utilisant accept(2)
+	L'argument backlog définit une longueur maximale jusqu'à laquelle la file 
+	des connexions en attente pour sockfd peut croître. 
+	Si une nouvelle connexion arrive alors que la file est pleine, 
+	le client reçoit une erreur indiquant ECONNREFUSED. 
+	SOMAXCONN defines the maximum number you're allowed to pass to listen(), depends on system*/
+	listen(m_sock,SOMAXCONN); 
+
+	/* Since we start with only one socket, the listening socket,
+	   it is the highest socket so far. */
+	m_highsock = m_sock;
 
 
 	return 0;
 }
 
 int Listener::run() {
+	while (m_run) {
+
+	}
 
 	return 0;
 }
+
--- listener.hpp
+++ listener.hpp
@@ -5,6 +5,9 @@
 #include <netinet/in.h>
 #include <errno.h>
 #include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
 
 class Listener {
 
@@ -13,12 +16,17 @@ class Listener {
 		int init();
 		int run();
 	
+	protected:
+		void set_non_blocking();
+
 	private:
-		const char *m_ip;
+		struct sockaddr_in m_address; // /* bind info structure */ need to have IP defined? see with config
 		int			m_port;
 		int			m_sock; /* The socket file descriptor for our "listening" socket */
-		int			*m_socket; /* Array of connected sockets so we know who we are talking to */ // A VOIR
+		int			*m_connect_list; /* Array of connected sockets so we know who we are talking to */ // A VOIR
 		fd_set		set; /* Socket file descriptors we want to wake up for, using select() */
+		int			m_highsock;
+		bool		m_run;
 
 	/* 1 listening socket + other socket fds */
 };
