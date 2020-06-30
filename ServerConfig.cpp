cf0312d0a6e24fb981ae2fc62236a335b4d401fb
jukazekajou <jerome.d274@gmail.com>
2020-06-30 23:59:03 +0200

    parser en bonne voie

--- ServerConfig.cpp
+++ ServerConfig.cpp
@@ -5,11 +5,13 @@ ServerConfig::ServerConfig()
 	_root = "";
 	_errors = "";
 	_client_body_size = 0;
-	_serverName = "";
+	_server_name = "";
 	_listen = 0;
-	_host = 0;
-	_directory_listing = 0;
+	_host = "";
+	_allow_directory_listing = 0;
+	_allow_uploaded = 0;
 	_uploaded_files_root = "";
+	// config = NULL;
 }
 
 ServerConfig::ServerConfig(const ServerConfig& serverConfig)
@@ -17,52 +19,161 @@ ServerConfig::ServerConfig(const ServerConfig& serverConfig)
 	_root = serverConfig._root;
 	_errors = serverConfig._errors;
 	_client_body_size = serverConfig._client_body_size;
-	_serverName = serverConfig._serverName;
+	_server_name = serverConfig._server_name;
 	_listen = serverConfig._listen;
 	_host = serverConfig._host;
-	_directory_listing = serverConfig._directory_listing;
+	_allow_directory_listing = serverConfig._allow_directory_listing;
+	_allow_uploaded = serverConfig._allow_uploaded;
 	_uploaded_files_root = serverConfig._uploaded_files_root;
 }
+
 bool ServerConfig::setConfig(std::string s)
 {
 	//something like this
 	fmap map;
 	initiate_map(map);
+	std::string key;
+	std::string value;
+	int i;
 
 	while (s.size() > 0 && s != "}")
 	{
-		int i = 0;
+		i = 0;
 		s = s.substr(s.find_first_of(ALPHACHAR));
 		i = s.find(" ");
-		std::string key = s.substr(0, i);
+		key = s.substr(0, i);
 		s = s.substr(i);
 		s = s.substr(s.find_first_of(ALPHACHAR));
-		std::string value = s.substr(0, s.find(';'));
+		value = s.substr(0, s.find(';'));
 		std::cout << "key: " << key << std::endl;
 		std::cout << "value: " << value << std::endl;
 		//map[s.substr(0, i)](s.substr(i, s.find(';')));
-	//	fmap::const_iterator it;
-	//	if (key == "root")
-	//		((*it).second)(value);
-		_root = "lol";
+		if (map[key] == NULL)
+			throw (std::runtime_error("Parsing error: Unknown option: " + key));
+			//Func f = it->second;
+		//	(this->*it->second)(value);
+		try
+		{
+			(this->*(map[key]))(value);
+		}
+		catch (...)
+		{
+			throw (std::runtime_error("Parsing error: Unknown value: " + value + " for option: " + key));
+			return (false);
+		}
+			//*f(value);
+			//(this->(*(it->second)))(value);
+		//_root = "lol";
 		s = s.substr(s.find(';') + 1);
 		//std::cout << "s here: " << s << std::endl;
 		//s += s.find(';');
 	}
 
-
+	print_data();
 	return (true);
 }
 
 void ServerConfig::initiate_map(fmap& map)
 {
-	map["root"] = &parse_root;
+	//map["root"] = &ServerConfig::parse_root;
+	map["root"] = &ServerConfig::parse_root;
+	map["errors"] = &ServerConfig::parse_errors;
+	map["client_body_size"] = &ServerConfig::parse_client_body_size;
+	map["server_name"] = &ServerConfig::parse_server_name;
+	map["listen"] = &ServerConfig::parse_listen;
+	map["host"] = &ServerConfig::parse_host;
+	map["method"] = &ServerConfig::parse_method;
+	map["directory_listing"] = &ServerConfig::parse_allow_directory_listing;
+	map["default_directory_answer_file"] = &ServerConfig::parse_default_directory_answer_file;
+	map["uploaded_files"] = &ServerConfig::parse_allow_uploaded;
+	map["uploaded_files_root"] = &ServerConfig::parse_uploaded_files_root;
 }
 
-void parse_root(std::string b)
+void ServerConfig::parse_root(std::string b)
 {
-	//_root = b.substr(0, b.find(';'));
+	_root = b;
 	//std::cout << _root << std::endl;
-	std::cout << "lol\n";
+	//std::cout << "root here: " << _root << std::endl;
 	//return (true);
+}
+
+void ServerConfig::parse_errors(std::string b)
+{
+	_errors = b;
+}
+
+void ServerConfig::parse_client_body_size(std::string b)
+{
+	_client_body_size = stoi(b);
+}
+
+void ServerConfig::parse_listen(std::string b)
+{
+	_listen = stoi(b);
+}
+
+
+void ServerConfig::parse_host(std::string b)
+{
+	_host = b;
+}
+
+void ServerConfig::parse_method(std::string b)
+{
+	std::string s;
+	_accepted_method.push_back("test");
+	/*while (b.size() > 1)
+	{
+		s = b.substr(0, b.find(','));
+		_accepted_method.push_back(b);
+		if (b.find(','))
+			b = b.substr(b.find(',') + 1);
+		else
+			b = b.substr(b.)
+		std::cout << b << std::endl;
+	}*/
+}
+
+void ServerConfig::parse_allow_directory_listing(std::string b)
+{
+	if (b == "yes")
+		_allow_directory_listing = true;
+	else if (b == "no")
+		_allow_directory_listing = false;
+	else
+		throw(std::runtime_error("Allow_directory_listing: Must be yes or no"));
+}
+
+void ServerConfig::parse_default_directory_answer_file(std::string b)
+{
+	_default_directory_answer_file = b;
+}
+void ServerConfig::parse_allow_uploaded(std::string b)
+{
+	if (b == "yes")
+		_allow_uploaded = true;
+	else if (b == "no")
+		_allow_uploaded = false;
+	else
+		throw(std::runtime_error("Allow_uploaded: Must be yes or no"));
+}
+
+void ServerConfig::parse_uploaded_files_root(std::string b)
+{
+	_uploaded_files_root = b;
+}
+
+void ServerConfig::parse_server_name(std::string b)
+{
+	_server_name = b;
+}
+
+void ServerConfig::print_data()
+{
+	std::cout << "server_name " << _server_name << " listen: " <<
+	_listen << " host: " << _host << " root: " << _root << " errors: " << _errors
+	<< " client body stuff: " << _client_body_size << " accepted method: " << _accepted_method.front()
+	<< " Allow_uploaded: " << _allow_uploaded << " uploaded_files_root: " << _uploaded_files_root
+	<< " Allow_directory_listing: " << _allow_directory_listing <<
+	" default_directory_answer_file: " << _default_directory_answer_file << std::endl;
 }
--- ServerConfig.hpp
+++ ServerConfig.hpp
@@ -9,16 +9,20 @@
 
 #define ALPHACHAR "ABCEDFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789" // Degueulasse
 
-void parse_root(std::string b);
+
+/*
+** this class should serve to parse a config struct listed in Data
+** The architecture isn't definitive yet
+*/
 
 class ServerConfig
 {
 	/*
 	** maybe a map of functions
 	*/
-	typedef void (*FUNC)(std::string s);
+	typedef void (ServerConfig::*Func)(std::string s);
    	//typedef std::map<std::string, FUNCPTR> FuncMap;
-	typedef std::map<std::string, void (*)(std::string s)> fmap;
+	typedef std::map<std::string, Func> fmap;
 	//typedef bool (ServerConfig::*)(std::string) fparse;
 
 	public:
@@ -33,25 +37,49 @@ class ServerConfig
 	void initiate_map(fmap&);
 
 
+
+	void print_data(); // utilitary, for testing only
+
 	// getters:
 
 	private:
 	//paths
+	/*
+	** not sure if host and listen and stuff should be strings or int
+	** Should i check if ips are valid here, or elsewhere?
+	*/
+
+	std::string _server_name;
 	std::string _root; //-> root path of the server like www/
 	std::string _errors; // ->root of default errors directory
 	unsigned int _client_body_size;// --> don't know what it is
 
 	//routing
-	std::string _serverName; // SERVER_NAME, typing it should be equivalent to host i guess
 	unsigned int _listen; // the listen port
-	unsigned int _host; // local host or something else
+	std::string _host; // local host or something else
 
 	std::list<std::string> _accepted_method; // list of accepted method
-	bool _directory_listing;
+	bool _allow_directory_listing;
 	std::string _default_directory_answer_file; // ????
 
 	bool		_allow_uploaded;
 	std::string	_uploaded_files_root;
+
+	std::string _cgi_extension;
+	std::string _cgi_root;
+
+	void parse_root(std::string b);
+	void parse_client_body_size(std::string b);
+	void parse_errors(std::string b);
+	void parse_listen(std::string);
+	void parse_host(std::string);
+	void parse_method(std::string);
+	void parse_directory(std::string);
+	void parse_allow_directory_listing(std::string);
+	void parse_default_directory_answer_file(std::string);
+	void parse_allow_uploaded(std::string);
+	void parse_uploaded_files_root(std::string);
+	void parse_server_name(std::string);
 
 
 };
