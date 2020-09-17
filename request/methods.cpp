b729eb3990aae697fdc375fa6f9183d1605ff0ea
bothilie <bothilie@42.student.fr>
2020-09-17 18:30:50 +0200

    default.conf

--- request/methods.cpp
+++ request/methods.cpp
@@ -32,7 +32,7 @@ int Request::forking()
 	if ((path = ft_strjoin(dir_cgi, m_url.c_str())) == NULL)
 		return (-1);
 	_head_req.PATH_TRANSLATED = path;
-	_head_req.PATH_INFO = path;
+	_head_req.PATH_INFO = m_url;
 	std::cout << "path cgi: " << path << std::endl;
 	std::ifstream f(path);
 	if (!f.good())
