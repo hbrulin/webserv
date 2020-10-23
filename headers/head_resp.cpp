1e36ba96ab6b0eb25df39317dea5cd85fe8588f2
babaoriley <helenebrulin@live.fr>
2020-10-23 11:32:41 +0200

    removal streams for head_resp

--- headers/head_resp.cpp
+++ headers/head_resp.cpp
@@ -41,49 +41,51 @@ std::string Head_resp::getContentLength(const char *path)
 
 std::string Head_resp::getBuffer(int code, const char *fichier, std::vector<std::string> methods, std::string _method)
 {
+	std::string ret;
 	//std::cout << "!!!!!" << fichier << std::endl;
-    std::ostringstream oss;
-	oss << DEF_PROTOCOL << " " << code;
-	oss << codeTab.find(code)->second;
-	oss << CACHE_STR;
-    oss << CONTENT_T_STR << "text/html" << "\r\n";
-	oss << CONTENT_LANG_STR << CONTENT_LANGUAGE << "\r\n";
+    //std::ostringstream oss;
+	ret = (std::string)DEF_PROTOCOL + " ";
+	ret += std::to_string(code) + codeTab.find(code)->second;
+	ret += CACHE_STR;
+    ret += (std::string)CONTENT_T_STR + "text/html\r\n";
+	ret += CONTENT_LANG_STR + CONTENT_LANGUAGE + "\r\n";
 	if (_method == PUT || (_method == POST && getContentLength(fichier) == ""))
-		oss << CONTENT_L_STR << "0" << "\r\n";
+		ret+= (std::string)CONTENT_L_STR + "0\r\n";
 	else
-		oss << CONTENT_L_STR << getContentLength(fichier) << "\r\n";
+		ret+= (std::string) CONTENT_L_STR + getContentLength(fichier) + "\r\n";
 	//oss << "Transfer-Encoding: deflate\r\n";
-	oss << CONTENT_LOC_STR << fichier << "\r\n";
+	ret += CONTENT_LOC_STR + (std::string)fichier + "\r\n";
 	/*if (WWW_AUTHENTICATE != NULL)
 		oss << "WWW-Authenticate: " << WWW_AUTHENTICATE[0]<< " " << WWW_AUTHENTICATE[1]  << "\r\n";*/
-    oss << DATE_STR << this->getDate() << "\r\n";
-    oss << SERVER_STR << SERVER << "\r\n";
-    oss << LAST_MOD_STR << this->getLastModified(fichier) << "\r\n";
-    if (code == 503) // Not available
-        oss << RETRY_STR << RETRY_AFTER << "\r\n";
+    ret += DATE_STR + this->getDate() + "\r\n";
+    ret += SERVER_STR + SERVER + "\r\n";
+    ret += LAST_MOD_STR + this->getLastModified(fichier) + "\r\n";
+    if (code == 503) 
+        ret += RETRY_STR + RETRY_AFTER + "\r\n";
 	if (code == 405)
 	{
-		oss << ALLOW_STR << ALLOW << std::endl;
-		oss << SERVER_METHODS_STR;
+		ret += ALLOW_STR + ALLOW + "\r\n";
+		ret += SERVER_METHODS_STR;
 		 for (std::vector<std::string>::iterator it = methods.begin(); it != methods.end(); it++)
-		 	oss << *it << ", ";
-		oss << "\r\n";
+		 	ret += *it + ", ";
+		ret += "\r\n";
 	}
-	oss << "\r\n";
-    return oss.str();
+	ret += "\r\n";
+    return ret;
 }
 
 std::string Head_resp::getBuffer_cgi(int code, std::string m_body, std::string X_head)
 {
-    std::ostringstream oss;
-	oss << DEF_PROTOCOL << " " << code;
-	oss << codeTab.find(code)->second;
-	oss << CONTENT_L_STR << m_body.size() << "\r\n";
-    oss << CONTENT_T_STR << CONTENT_TYPE << "\r\n";
-	oss << DATE_STR << this->getDate() << "\r\n";
-	oss << SERVER_STR << SERVER << "\r\n";
+    //std::ostringstream oss;
+	std::string ret;
+	ret = (std::string)DEF_PROTOCOL + " ";
+	ret += std::to_string(code) + codeTab.find(code)->second;
+	ret += CONTENT_L_STR + std::to_string(m_body.size()) + "\r\n";
+    ret += CONTENT_T_STR + CONTENT_TYPE + "\r\n";
+	ret += DATE_STR + this->getDate() + "\r\n";
+	ret += SERVER_STR + SERVER + "\r\n";
 	if (X_head != "")
- 	 	oss << "http-" << X_head << "\r\n";
-	oss << "\r\n";
-    return oss.str();
+ 	 	ret += "http-" + X_head + "\r\n";
+	ret += "\r\n";
+    return ret;
 }
